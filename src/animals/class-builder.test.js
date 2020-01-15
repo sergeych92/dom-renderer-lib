ca64963a05822908ef9bbeb1274ac216b3a335c8
Sergei Cherednichenko <sergeych92@gmail.com>
2020-01-15 15:26:26 +0100

    implement inheritance and constructor calls

---
+++ src/animals/class-builder.test.js
@@ -0,0 +1,21 @@
+import { ClassBuilder } from './class-builder';
+
+test('create simple class', () => {
+    var Animal = ClassBuilder({
+        name: 'Animal',
+        constructor: function (favoriteFood) {
+            this.legs = 4;
+            this.__favoriteFood = favoriteFood;
+        }
+    }).build();
+
+    expect(Animal).toBeTruthy();
+    expect (typeof Animal).toEqual('function');
+    expect(Bird.prototype).toEqual(Object.prototype);
+
+
+    const animal = new Animal('sunflower seeds');
+    expect(animal).toBeTruthy();
+    expect(animal.legs).toEqual(4);
+    expect(animal.__favoriteFood).toEqual('sunflower seeds');
+});
